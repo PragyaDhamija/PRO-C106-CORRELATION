b44978c992b3c0bd03e1dc8d93f7107d0b6b770c
PragyaDhamija <72442929+PragyaDhamija@users.noreply.github.com>
2022-03-08 16:55:05 +0530

    Add files via upload

---
+++ Project/marksStd.py
@@ -0,0 +1,21 @@
+import plotly.express as px
+import numpy as np 
+import csv
+
+def getDataSource(dp):
+    marks = []
+    days = []
+    with open(dp) as d:
+        df = csv.DictReader(d)
+        for i in df:
+            marks.append(float(i["Marks In Percentage"]))
+            days.append(i["Days Present"])
+    return {"x":marks, "y":days}
+
+def findCorrelation(ds):
+    c = np.corrcoef(ds["x"],ds["y"])
+    print(c[0,1])
+def setup():
+    gds = getDataSource("Project\Student Marks vs Days Present.csv")
+    findCorrelation(gds)    
+setup()
